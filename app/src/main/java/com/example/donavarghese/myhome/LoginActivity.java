141f10914989e41b41d20b0205def83739b7d13c
AppCoder007 <donavarghese92@gmail.com>
2017-12-07 20:29:23 -0500

    some changes

--- app/src/main/java/com/example/donavarghese/myhome/LoginActivity.java
+++ app/src/main/java/com/example/donavarghese/myhome/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.donavarghese.myhome;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +12,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+    public void createAccount(View view){
+        Intent intent = new Intent(this, RegistrationActivity.class);
+       // EditText editText = (EditText) findViewById(R.id.editText);
+      //  String message = editText.getText().toString();
+     //   intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    public void getPassword(View view){
+        Intent intent = new Intent(this, GetPasswordActivity.class);
+       // EditText editText = (EditText) findViewById(R.id.editText);
+       // String message = editText.getText().toString();
+       // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+
 }
