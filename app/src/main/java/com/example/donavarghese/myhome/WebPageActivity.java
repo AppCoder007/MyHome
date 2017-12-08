21191b3604c5a51dc62c210dd43050eabbcedcbb
AppCoder007 <donavarghese92@gmail.com>
2017-12-08 00:32:22 -0500

    some big changes

--- app/src/main/java/com/example/donavarghese/myhome/WebPageActivity.java
+++ app/src/main/java/com/example/donavarghese/myhome/WebPageActivity.java
@@ -1,8 +1,10 @@
 package com.example.donavarghese.myhome;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -32,4 +34,11 @@ public class WebPageActivity extends AppCompatActivity {
         myWebView.loadUrl("http://www.google.com");
 
     }
+
+   public void logout(View view){
+       Intent intent = new Intent(this, LoginActivity.class);
+
+       startActivity(intent);
+   }
+
 }
